81c3b235dda7a69364128696495a5fd5fd336604
NavalTech <ebmmcdowell@gmail.com>
2022-01-26 20:15:36 -0600

    finance functions

---
+++ week2/finance_functions.py
@@ -0,0 +1,17 @@
+# Definitions for finance related functions
+#
+# Author: Ernest B McDowell
+# Date: 26JAN2022
+#
+
+def compound_interest(principal, rate, length_of_investment, compound_frequency):
+    # A = P*(1+r/n)**(n*t)
+    amount = principal*(1+(rate/100)/compound_frequency)**(compound_frequency*length_of_investment) 
+    amount = round(amount,2)
+    return amount
+
+
+def calc_stock_apr(start_price, end_price, length_of_investment):
+    compound_frequency = 12
+    rate = compound_frequency*((end_price/start_price)**(1/(compound_frequency*length_of_investment))-1)
+    return rate
---
+++ week2/test.py
@@ -0,0 +1,10 @@
+# Test for finance functions
+#
+# Author: Ernest B McDowell
+# Date: 26JAN2022
+
+import finance_functions as finance
+
+calc = finance.compound_interest(200, 10, 10, 52)
+
+print(calc)
