c18bae2d0f9518b6a2fcbee4e339b01bedf2d1b8
NavalTech <ebmmcdowell@gmail.com>
2022-01-27 16:56:14 -0600

    removed file update others

--- week2/stock_apr.py
+++ week2/stock_apr.py
@@ -8,10 +8,10 @@
 # Compound Formula is rate = compound_frequency*((amount/principal)**(1/(compound_frequency*time_of_investment))-1)
 
 stock_start = float(input('Enter the starting price of the stock: '))
-compound_frequency = float(input(' Please enter the frequency at which interest will be accrued: '))
+compound_frequency = float(input('Please enter the frequency at which interest will be accrued: '))
 length_of_investment = float(input('Please eneter the length of time that you owned the stock: '))
 stock_end = float(input('Enter the stock ending price: '))
 rate = compound_frequency*((stock_end/stock_start)**(1/(compound_frequency*length_of_investment))-1)
 rate = round(rate*100,2)
 
-print('The average APR over '+str(length_of_investment)+' years is : '+str(rate)+' %')
+print('The average APR over '+str(length_of_investment)+' years is : '+str(rate)+'%')
