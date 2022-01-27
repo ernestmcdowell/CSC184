bcd293bd9e88a34b217f0d93cf9323606f45ada6
NavalTech <ebmmcdowell@gmail.com>
2022-01-26 20:36:23 -0600

    fix typos

---
+++ week2/homework2_1.py
@@ -0,0 +1,7 @@
+# Title: Homework2_1 for CSC184
+#
+# Description: Take user input of 5 stocks and calculate APR over a 5 year period
+#
+# Author: Ernest B McDowell
+# Date: 26JAN2022
+#
--- week2/stock_apr.py
+++ week2/stock_apr.py
@@ -8,7 +8,7 @@
 # Compound Formula is rate = compound_frequency*((amount/principal)**(1/(compound_frequency*time_of_investment))-1)
 
 stock_start = float(input('Enter the starting price of the stock: '))
-compound_frequency = float(input(' Please enter the frequency at which interested will be accrued: '))
+compound_frequency = float(input(' Please enter the frequency at which interest will be accrued: '))
 length_of_investment = float(input('Please eneter the length of time that you owned the stock: '))
 stock_end = float(input('Enter the stock ending price: '))
 rate = compound_frequency*((stock_end/stock_start)**(1/(compound_frequency*length_of_investment))-1)
